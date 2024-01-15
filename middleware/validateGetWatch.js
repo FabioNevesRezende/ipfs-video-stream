eea054e702b6301402218148b297c375cd495129
Fabio <fnr9090@protonmail.com>
2024-01-15 20:24:13 -0300

    fix cid regex validation

--- middleware/validateGetWatch.js
+++ middleware/validateGetWatch.js
@@ -2,7 +2,8 @@ import { query, validationResult } from 'express-validator'
 import {goPage} from '../utils.js'
 
 const validateGetWatch = [
-    query('filehash').exists().matches(/Qm[A-Za-z0-9]{44}$/).withMessage('Invalid cid'),
+    // regex source: https://stackoverflow.com/a/67176726
+    query('filehash').exists().matches(/Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}/).withMessage('Invalid cid'),
     function(req,res,next) { 
         console.log('running middleware handle get watch')
         const errors = validationResult(req);
--- middleware/validateReact.js
+++ middleware/validateReact.js
@@ -2,7 +2,8 @@ import { body, validationResult } from 'express-validator'
 import {goPage} from '../utils.js'
 
 const validateReact = [
-    body('cid').exists().matches(/Qm[A-Za-z0-9]{44}$/).withMessage('Invalid cid'),
+    // regex source: https://stackoverflow.com/a/67176726
+    body('cid').exists().matches(/Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}/).withMessage('Invalid cid'),
     body('reaction').exists().isInt({min: 0, max: 1}).escape().trim().withMessage('Invalid reaction'),
     function(req,res,next) { 
         console.log('running middleware validate react')
